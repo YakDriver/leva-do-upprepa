339e127e7042e9e8d5e85696b6a54c4652d39ebf
Dirk Avery <dirk.avery@gmail.com>
2019-02-04 18:31:53 -0500

    Test with newest versions

---
+++ test_install6.ps1
@@ -0,0 +1,16 @@
+cd c:\temp
+pip install virtualenv
+virtualenv venv
+venv\Scripts\activate
+python -m pip install --upgrade "pip==18.1"
+git clone --branch develop https://github.com/plus3it/watchmaker.git --recursive
+git clone --branch rehook https://github.com/YakDriver/pyppyn.git
+git clone --branch rehook https://github.com/YakDriver/gravitybee.git
+cd pyppyn
+pip install --editable .
+cd ..\gravitybee
+pip install --editable .
+cd ..\watchmaker
+pip install --editable .
+gravitybee --src-dir src --sha file --with-latest --extra-data static --extra-pkgs boto3 --extra-modules boto3
+cd .gravitybee\dist\latest
